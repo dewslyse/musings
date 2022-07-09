5cba74afe44248a817b6cf4bb001d409f62358d3
dewslyse <dewslyse@gmail.com>
2022-07-09 22:14:00 +0000

    Update root route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get 'pages/home', to: 'pages#home'
-  
-  get 'pages/about', to: 'pages#about'
+  root 'pages#home'
+
+  get 'about', to: 'pages#about'
 end
