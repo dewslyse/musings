9c280f5289d35e2cd089eea976d716bbf1d06b5d
dewslyse <dewslyse@gmail.com>
2022-07-10 17:36:56 +0000

    Add edit article action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
 
@@ -11,6 +15,17 @@ class ArticlesController < ApplicationController
       redirect_to article_path(@article)
     else
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      flash[:notice] = 'Article updated successfully'
+      redirect_to article_path(@article)
+    else
+      render 'edit'
     end
   end
 
