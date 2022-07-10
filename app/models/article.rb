4a5ef40474f8f973de729aa99b48566100763b67
dewslyse <dewslyse@gmail.com>
2022-07-10 16:55:37 +0000

    Create article from UI

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-  validates :title, presence: true, length: { minimum: 5, maximum: 100 }
-  validates :description, presence: true, length: { minimum: 10, maximum: 240 }
+  validates :title, presence: true, length: { minimum: 3, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 5, maximum: 240 }
 end
