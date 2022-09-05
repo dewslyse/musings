308cc6d05c8b3250d233263cb1fd369183a77530
dewslyse <dewslyse@gmail.com>
2022-09-05 23:38:09 +0000

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+articles = Article.create([
+  { title: "Learning to Code: Microverse vs. Self-Study", description: "In the age of technology, learning to code has never been more important. We're going to compare two different methods for learning to code: Microverse vs. self-study. Which one is better?" },
+  { title: "Is Microverse Legit?", description: "Is Microverse legit? Students say so. Read about their experience and see why they love the program." },
+  { title: "10 Advanced Javascript Code Challenges", description: "10 Advanced JavaScript Code Challenges that you can use to improve your coding skills." },
+  { title: "How to Work With JSON in Javascript", description: "Learn how to work with JSON in Javascript. JSON stands for JavaScript Object Notation, and it is an object-oriented data exchange format used to transmit data from one program to another." },
+  { title: "Microverse and Tunga Partnership", description: "We're excited to announce our partnership with Tunga to be able to help more talented people in Africa connect to life-changing opportunities and excel in international tech careers." }
+])
