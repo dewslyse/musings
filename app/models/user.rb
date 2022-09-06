369bb8090ac9fbb6e1f237c5f4d90c1cbb39b282
dewslyse <dewslyse@gmail.com>
2022-09-06 23:56:09 +0000

    Fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,5 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false },
                     format: { with: VALID_EMAIL_REGEX }
 
-  has_secure_password                    
+  has_secure_password
 end
