4500af04656bb539f9db6ea1cf06eae834719f79
dewslyse <dewslyse@gmail.com>
2022-09-06 00:17:07 +0000

    Require user to create article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user = User.first
 
     if @article.save
       flash[:success] = 'Article created successfully'
--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,5 @@ class Article < ApplicationRecord
 
   validates :title, presence: true, length: { minimum: 3, maximum: 100 }
   validates :description, presence: true, length: { minimum: 5, maximum: 1000 }
+  validates :user_id, presence: true
 end
