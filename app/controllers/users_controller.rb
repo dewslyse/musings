2b00b22c9df202e7bab6802ab7e05240a5a61d78
dewslyse <dewslyse@gmail.com>
2022-09-18 00:19:50 +0000

    Add pagination to user articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,6 +33,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   private
