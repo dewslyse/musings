bf2a9e9bb6c822d18a15ec7c61dab16ca9bde2fa
dewslyse <dewslyse@gmail.com>
2022-09-19 22:23:07 +0000

    Fix linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,9 +12,9 @@ class ApplicationController < ActionController::Base
   end
 
   def require_user
-    unless logged_in?
-      flash[:danger] = 'You must be logged in to perform that action'
-      redirect_to root_path
-    end
+    return if logged_in?
+
+    flash[:danger] = 'You must be logged in to perform that action'
+    redirect_to root_path
   end
 end
