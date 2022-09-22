74e3ad27f0c0240569e3a4113823d62d920aa79b
dewslyse <dewslyse@gmail.com>
2022-09-22 14:48:58 +0000

    Add integration test for invalid submission

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -12,4 +12,15 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     assert_template 'categories/index'
     assert_match 'sports', response.body
   end
+
+  test 'invalid submission results in failure' do
+    get new_category_path
+    assert_template 'categories/new'
+
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: ' ' } }
+    end
+    assert_template 'categories/new'
+    assert_select 'div.alert-danger'
+  end
 end
