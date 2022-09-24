c9b0aac35e70d63847db01025ac23d83a39fe94e
dewslyse <dewslyse@gmail.com>
2022-09-24 18:48:55 +0000

    Create article_categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_21_223938) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_24_183628) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
