64d1ad07151ee8c6fc7e1a822d1aac1842c7eaf3
dewslyse <dewslyse@gmail.com>
2022-09-25 10:16:54 +0000

    Update category show page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -19,7 +19,10 @@ class CategoriesController < ApplicationController
     end
   end
 
-  def show; end
+  def show
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 3)
+  end
 
   private
 
